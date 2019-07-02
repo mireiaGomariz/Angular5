1a941ddc8e8453d7bfcad5e4a3f2ecfbcd3affc5
mireiaGomariz <mireiapgomariz@gmail.com>
2019-07-02 17:54:55 +0200

    Carga de componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { VideojuegosComponent } from './videojuegos/videojuegos.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideojuegosComponent
   ],
   imports: [
     BrowserModule,
