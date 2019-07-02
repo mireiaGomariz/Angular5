c225a408d8fb9ed7a27d700c684536caa0684933
mireiaGomariz <mireiapgomariz@gmail.com>
2019-07-02 18:18:17 +0200

    Añadir más propiedades al Componente

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -4,9 +4,11 @@ import { Component } from '@angular/core'
   selector: 'videojuegos',
   template: `
     <h2>Componente de {{ nombre }} </h2>
+    <h3>El mejor juego es {{ mejor_juego }} </h3>
    `
 })
 
 export class VideojuegosComponent {
   public nombre = 'Videojuegos 2018';
+  public mejor_juego = 'Rdr2';
 }
