2312e824b75263760a2de54a5fd618cf44ef2454
mireiaGomariz <mireiapgomariz@gmail.com>
2019-07-03 17:08:59 +0200

    Bucle *ngIf *ngFor

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -4,11 +4,32 @@ import { Component } from '@angular/core'
   selector: 'videojuegos',
   template: `
     <h2>Componente de {{ nombre }} </h2>
-    <h3>El mejor juego es {{ mejor_juego }} </h3>
+    <h3 *ngIf="mostrar_retro !=true">El mejor juego es {{ mejor_juego }} </h3>
+    <h3 [style.background]="color"
+      *ngIf="mostrar_retro">
+      El mejor juego retro {{ mejor_juego_retro }}
+    </h3>
+    <h2>Listado de Videojuegos</h2>
+    <ul>
+      <li *ngFor = "let game of videojuegos">{{game}}</li>
+    </ul>
    `
 })
 
 export class VideojuegosComponent {
   public nombre = 'Videojuegos 2018';
   public mejor_juego = 'Rdr2';
+  public mejor_juego_retro = 'Super Mario 64';
+  public mostrar_retro = true;
+  public color = "yellow";
+  public videojuegos = [
+    'Overwatch',
+    'Assasins Creed',
+    'Bloodborne',
+    'Call of Duty'
+  ]
 }
+
+
+// Para mostrar el color con un atributo en corchetes deberemos añadir una propiedad a color o bien podremos usar
+// una string añadiendo comillas simple [style.background]= "'red'"
